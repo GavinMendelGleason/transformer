124f75dcc95f3330d939698be51dfe263371e910
Gavin Mendel-Gleason <jacobian@gmail.com>
2020-05-14 13:38:44 +0200

    Adding test file and completing package

--- prolog/optimise.pl
+++ prolog/optimise.pl
@@ -1,55 +1,82 @@
-:- module(optmise, [
-              optimise/1
+:- module(optimise, [
+              optimise/1,
+              optimise_all/0,
+              set_optimise_options/1
           ]).
-
 
 :- use_module(reify).
 :- use_module(equality_optimise).
+:- use_module(library(option)).
 
 :- dynamic '$program'/1.
 :- dynamic '$participating_clause'/1.
+:- dynamic '$options'/1.
+
+:- op(920,fy, *).
+*_.
+
+'$options'([equality_cut(true)]).
 
 optimise(Predicate) :-
-    assertz('$participating_clause',Predicate).
+    assertz('$participating_clause'(Predicate)).
+
+optimise_all :-
+    assertz('$participating_clause'(_)).
 
 module_wants_guard(Module) :-
     Module \= optimise,
     predicate_property(Module:optimise(_), imported_from(optimise)).
 
-participating_predicate(Pred,Program) :-
-    predicate_clauses(Program, Pred, _Clauses).
+participating_clause(Clause) :-
+    Clause = clause(Pred,_,_),
+    '$participating_clause'(Pred).
 
 get_program(Program) :-
     bagof(Clause,
           '$program'(Clause),
           Program).
 
-transformed_clauses(Clauses) :-
-    get_program(Program),
-    findall(New_Clauses,
-            (   member(Clause, Program)
-            ->  (   predicate_clauses(Program, Pred, Predicate_Clauses) ->
-                    (   participating_predicates(Pred,Program)
-                    ->  optimise_clauses(Predicate_Clauses,New_Clauses)
-                    ;   Predicate_Clauses = New_Clauses)
-                ;   New_Clauses = Predicate_Clauses)
-            ;   New_Clauses = true % huh?
-            Clauses).
+set_optimise_options(Options) :-
+    get_optimise_options(Old_Options),
+    merge_options(Options,Old_Options,New_Options),
+    retractall('$options'(_)),
+    assertz('$options'(New_Options)).
 
+get_optimise_options(Options) :-
+    '$options'(Options).
+
+check_option(Option) :-
+    get_optimise_options(Options),
+    member(Option,Options).
+
+optimise_clauses(Clauses, Optimised) :-
+    check_option(equality_cut(true)),
+    equality_optimise(Clauses,Optimised).
+
+transformed_terms(Terms) :-
+    get_program(Program),
+    findall(New_Terms,
+            (   predicate_clauses(Program, _Pred, Predicate_Clauses),
+                optimise_clauses(Predicate_Clauses,New_Clauses),
+                maplist(reify_reflect,New_Terms,New_Clauses)
+            ),
+            Terms_Sets),
+    append(Terms_Sets,Terms).
 
 user:term_expansion(begin_of_file, begin_of_file) :-
     % We don't want to process ourselves, so fail...
     prolog_load_context(module, Module),
     optimise:module_wants_guard(Module),
     retractall(optimise:'$program'(_)),
-user:term_expansion(end_of_file,Clauses) :-
+    retractall(optimise:'$participating_clause'(_)).
+user:term_expansion(end_of_file,Terms) :-
     prolog_load_context(module, Module),
     optimise:module_wants_guard(Module),
-    optimise:transformed_clauses(Clauses),
-    optimise:give_report.
+    optimise:transformed_terms(Terms).
 user:term_expansion(Term, No_Clause) :-
     prolog_load_context(module, Module),
     optimise:module_wants_guard(Module),
-    optimise:reify_reflect(Statement,Term),
+    optimise:reify_reflect(Term,Statement),
+    participating_clause(Statement),
     assertz(optimise:'$program'(Statement)),
     No_Clause = [].
---
+++ prolog/test_optimiser.pl
@@ -0,0 +1,21 @@
+:- module(test_optimiser,[]).
+
+:- use_module(optimise).
+
+:- optimise(p/3).
+
+p(x,y,z) :- q.
+p(x,w,z) :- r.
+p(x,y,q) :- s.
+
+p(x,y) :- s.
+p(z,w) :- s.
+
+q.
+q.
+
+r.
+
+s.
+
+
